972216725a26e3d7fbc48a445bba0e1471a6e4dd
Shabih Alam <96066877+mailingtoshabih@users.noreply.github.com>
2023-08-21 16:17:24 +0530

    Added interactive  filter tabs in question section

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -10,22 +10,49 @@ export const Questions = () => {
 
 
     const [questions, setQuestions] = useState([]);
+    const [tab, setTab] = useState(1);
+
+
+
+
 
 
     useEffect(() => {
 
         const search = async () => {
             try {
-                const res = await axios.get("https://api.stackexchange.com/2.2/questions?order=desc&sort=activity&filter=default&site=stackoverflow");
-                res && setQuestions(res.data.items)
+                const res = await axios.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&filter=default&site=stackoverflow");
+
+                switch (tab) {
+                    // intersting Tab
+                    case 1:
+                        const res = await axios.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&filter=default&site=stackoverflow");
+                        break;
+
+                    // /Featured tab
+                    case 2:
+                        const res2 = await axios.get("https://api.stackexchange.com/2.3/questions?order=desc&sort=activity&filter=default&site=stackoverflow");
+                        break;
+
+                    // Hot tab
+                    // Month tab
+                    // Week tab
+
+                    default:
+                        break;
+                }
+
+
+                res && setQuestions(res.data.items);
+
             }
             catch (e) {
                 console.log(e.message);
             }
         }
 
-        search();
-    }, [])
+        // search();
+    }, [tab])
 
 
 
@@ -41,17 +68,42 @@ export const Questions = () => {
             </div>
 
 
-            <div className='px-3 md:px-10 mt-10 mb-5 flex space-x-5 text-gray-500'>
+            {/* tab bar */}
 
-                <p className='p-1 px-4 bg-orange-500 text-white rounded-full'>
-                    All</p>
+            <div className='px-3 md:px-10 mt-10 mb-5 flex space-x-5 text-gray-500 overflow-y-auto'>
+
+                <p className={`${tab === 1 ? "bg-orange-600 text-white" : "text-gray-500"} p-1 px-4 my-auto rounded-full`}
+                    onClick={() => setTab(1)}>
+                    Interesting
+                </p>
+
 
                 <div className='mt-1 flex space-x-3'>
-                    <p className='cursor-pointer'>Featured</p>
 
-                    <p className='cursor-pointer'>Hot</p>
+                    <div className='cursor-pointer flex'
+                        onClick={() => setTab(2)}>
 
-                    <p className='cursor-pointer'>Weeks</p>
+                        <p className={`${tab === 2 ? "bg-orange-600 text-white" : "text-gray-500"} p-1 px-4 rounded-full`} >
+                            Featured
+                        </p>
+
+                        <span className='mx-1 p-1 px-2 border rounded-full text-orange-500 font-semibold'>
+                            234
+                        </span>
+
+                    </div>
+
+
+                    <p className={`${tab === 3 ? "bg-orange-600 text-white" : "text-gray-500"} p-1 px-4 rounded-full`}
+                        onClick={() => setTab(3)}>
+                        Hot
+                    </p>
+
+                    <p className={`${tab === 4 ? "bg-orange-600 text-white" : "text-gray-500"} p-1 px-4 rounded-full`}
+                        onClick={() => setTab(4)}>Week</p>
+
+                    <p className={`${tab === 5 ? "bg-orange-600 text-white" : "text-gray-500"} p-1 px-4 rounded-full`}
+                        onClick={() => setTab(5)}>Month</p>
 
                 </div>
 
